fdc8c3ecb6bb8e9abf71d823d4775c90545feb3f
Phia <sophia.francis@nesta.org.uk>
2025-12-19 17:06:16 +0000

    Update config.py

--- config.py
+++ config.py
@@ -14,3 +14,16 @@ class Config:
     # Thresholds
     CONFIDENCE_THRESHOLD = float(os.environ.get("CONFIDENCE_THRESHOLD", 0.8))
     STALE_DAYS = int(os.environ.get("STALE_DAYS", 14))
+
+class Brand:
+    # Nesta Core Palette
+    BLUE = "#0000FF"      # Core Blue
+    TEAL = "#0FA3A4"      # "Green" in your file, close to teal
+    AMBER = "#FFB703"     # "Yellow/Orange" equivalent for CTAs
+    NAVY = "#072033"      # "Navy" for text/headers
+    WHITE = "#FFFFFF"
+    
+    # Semantic Mapping for OCP Framework
+    COLOR_OPPORTUNITY = AMBER  # Opportunity = Discovery/Warning
+    COLOR_CAPABILITY = TEAL    # Capability = Resources/Go
+    COLOR_PROGRESS = BLUE      # Progress = Data/Core
